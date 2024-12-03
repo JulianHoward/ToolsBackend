52349019e3de48db9065388c060985e4b2e15e98
Julian Howard <julianhowardmontalvo@gmail.com>
2024-12-03 14:49:13 -0400

    Update database configuration for Clever Cloud MySQL connection

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "junction.proxy.rlwy.net", // Extraído de MYSQL_PUBLIC_URL
-    USER: "root",                   // Extraído de MYSQL_PUBLIC_URL
-    PASSWORD: "EGWUpQAIJUqjvKkqGoMouHYjXIIRiew", // Extraído de MYSQL_PUBLIC_URL
-    DB: "railway",                  // Nombre de la base de datos
-    PORT: 14310,                    // Puerto de MYSQL_PUBLIC_URL
+    HOST: "bkespsqgyld9fhgcgt5y-mysql.services.clever-cloud.com", // Extraído de MYSQL_PUBLIC_URL
+    USER: "upql3mwgd296zbba",                   // Extraído de MYSQL_PUBLIC_URL
+    PASSWORD: "xL6ICa1RIwjyvxJlzDWB", // Extraído de MYSQL_PUBLIC_URL
+    DB: "bkespsqgyld9fhgcgt5y",                  // Nombre de la base de datos
+    PORT: 3306,                    // Puerto de MYSQL_PUBLIC_URL
     dialect: "mysql",
 };
