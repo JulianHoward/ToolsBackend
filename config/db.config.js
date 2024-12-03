cc0b53b0041fcb317a673a92228618e03c4bd2a0
Julian Howard <julianhowardmontalvo@gmail.com>
2024-12-03 10:37:36 -0400

    db changes

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,7 @@
-
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "tools",
+    HOST: process.env.DB_HOST || "localhost",
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "root",
+    DB: process.env.DB_NAME || "tools",
     dialect: "mysql",
 }
